ed796fc7fa7e3cd765e3c87d29ec0b3d185578a2
myk225 <kyaseen228@gmail.com>
2024-04-28 13:44:32 +0530

    project-1 finished

--- client/main.js
+++ client/main.js
@@ -1,8 +1,16 @@
+
+
 console.log("lets start")
 const todoTitle=document.getElementById('todoTitle');
 const todoDesc=document.getElementById('todoDesc');
+
+const etodoTitle=document.getElementById('etodoTitle');
+const etodoDesc=document.getElementById('etodoDesc');
+
 const allTodos=document.getElementById("allTodos");
+const editSave=document.getElementById('editSave');
 let allRemoveBtns;
+let editTodoId;
 async function fetchTodos(){
     const userId=localStorage.getItem('userId');
     const response=await fetch(`/todos/getTodos/${userId}`,{
@@ -28,10 +36,14 @@ async function fetchTodos(){
         todoDesc.innerText=todo.desc;
         edit.innerText="edit";
         remove.innerText="remove";
-
+        edit.setAttribute('data-bs-toggle','modal');
+        edit.setAttribute('data-bs-target','#editModal');
         remove.classList.add("remove");
+        edit.classList.add("edit");
         remove.setAttribute('data-todoId',todo.id);
-        
+        edit.setAttribute('data-todoId',todo.id);
+        edit.setAttribute('data-title',todo.title);
+        edit.setAttribute('data-desc',todo.desc);
         div3.appendChild(edit);
         div3.appendChild(remove);
         div1.classList.add("todo");
@@ -40,7 +52,7 @@ async function fetchTodos(){
         div2.appendChild(todoTitle);
         div2.appendChild(todoDesc);
         div1.appendChild(div2);
-        div1.appendChild(div3);
+        div1.appendChild(div3); 
         console.log(allTodos)
 
         allTodos.appendChild(div1)
@@ -49,7 +61,7 @@ async function fetchTodos(){
     })
     
     allRemoveBtns=document.querySelectorAll(".remove");
-
+    allEditButtons=document.querySelectorAll(".edit");
     allRemoveBtns.forEach((rbtn)=>{
         rbtn.addEventListener('click',async()=>{
             let userId=localStorage.getItem('userId');
@@ -73,6 +85,17 @@ async function fetchTodos(){
         })
     })
     
+    // adding similar logic to all remove buttons
+
+    allEditButtons.forEach((ebtn)=>{
+        ebtn.addEventListener('click',async()=>{
+            let userId=localStorage.getItem('userId');
+            const todoId=ebtn.getAttribute('data-todoId');
+            editTodoId=todoId;     
+            etodoDesc.value=ebtn.getAttribute('data-desc');
+            etodoTitle.value=ebtn.getAttribute('data-title');
+        })
+    })
 
      
 }
@@ -106,5 +129,31 @@ addBtn.addEventListener('click',async()=>{
     fetchTodos();
 })
 
-// logic to delete a todo
+// logic to edit a todo
 
+
+editSave.addEventListener('click',async()=>{
+    let userId=localStorage.getItem('userId');
+    const todoId=editTodoId;
+
+    const response=await fetch(`/todos/updateTodo/${todoId}`,{
+        method:"PUT",
+        headers:{
+            "Content-Type": "application/json",
+        },
+        body:JSON.stringify({user_id:userId,title:etodoTitle.value,desc:etodoDesc.value})
+
+    })
+    const res=await response.json();
+
+    if(response.status==200){
+        fetchTodos();
+        alert(res.message);
+    }
+    alert(res.message);
+})
+
+document.getElementById('logout').addEventListener('click',()=>{
+    localStorage.removeItem('userId');
+    window.location.assign('/app/login.html')
+})
